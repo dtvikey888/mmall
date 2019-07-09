07dac66e4d2a3e216646daa18b2a0464ff0e4b83
dtvikey <dtvikey@qq.com>
2019-07-09 16:41:38 +0800

    Merge branch 'v1.0' of https://github.com/dtvikey888/mmall
    # Conflicts:
    #	pom.xml
    #	src/main/webapp/WEB-INF/web.xml

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,7 +1,13 @@
 package com.mmall.service.impl;
 
 import com.mmall.service.IFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @Author: dtvikey
@@ -9,7 +15,39 @@ import org.springframework.web.multipart.MultipartFile;
  * @Version 1.0
  */
 public class FileServiceImpl implements IFileService {
+
+    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
     public String upload(MultipartFile file,String path){
         String fileName = file.getOriginalFilename();
+        //扩展名
+        //abc.jpg
+        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
+        String uploadFileName = UUID.randomUUID()+toString()+"."+fileExtensionName;
+        logger.info("开始上传文件，上传的文件名:{},上传的路径：{}，新文件名：{}",fileName,path,uploadFileName);
+
+        File fileDir = new File(path);
+        if (!fileDir.exists()){
+            fileDir.setWritable(true);
+            fileDir.mkdirs();
+        }
+        File targetFile = new File(path,uploadFileName);
+        try {
+            file.transferTo(targetFile);
+            //文件已经上传成功
+
+            //todo 将targetFile 上传到我们的FTP服务器上
+
+            //todo 上传完成之后，删除upload下面的文件
+
+
+
+        } catch (IOException e) {
+            logger.error("上传文件异常",e);
+            return null;
+        }
+
+        return targetFile.getName();
     }
+
 }
