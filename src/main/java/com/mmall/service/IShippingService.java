defc67edc75cd86d1df41b8f4305ac11e01a8448
dtvikey <dtvikey@qq.com>
2019-08-07 09:22:26 +0800

    Merge branch 'v1.0' of github.com:dtvikey888/mmall into v1.0
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/mmall/service/IShippingService.java
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -1,16 +1,18 @@
 package com.mmall.service;
 
+import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Shipping;
 
-import javax.servlet.http.HttpSession;
-
 /**
- * @Author: dtvikey
- * @Date: 01/08/19 上午 11:03
- * @Version 1.0
+ * Created by geely
  */
 public interface IShippingService {
+
     ServerResponse add(Integer userId, Shipping shipping);
-    ServerResponse del(Integer userId,Integer shippingId);
+    ServerResponse<String> del(Integer userId, Integer shippingId);
+    ServerResponse update(Integer userId, Shipping shipping);
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+
 }
--- src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mmall.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.google.common.collect.Maps;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.ShippingMapper;
@@ -8,53 +10,68 @@ import com.mmall.service.IShippingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
-
 /**
- * @Author: dtvikey
- * @Date: 01/08/19 上午 11:03
- * @Version 1.0
+ * Created by geely
  */
 @Service("iShippingService")
 public class ShippingServiceImpl implements IShippingService {
+
 
     @Autowired
     private ShippingMapper shippingMapper;
 
     public ServerResponse add(Integer userId, Shipping shipping){
-
         shipping.setUserId(userId);
-        int rowCount=shippingMapper.insert(shipping);
-        if (rowCount>0){
+        int rowCount = shippingMapper.insert(shipping);
+        if(rowCount > 0){
             Map result = Maps.newHashMap();
             result.put("shippingId",shipping.getId());
             return ServerResponse.createBySuccess("新建地址成功",result);
         }
-
         return ServerResponse.createByErrorMessage("新建地址失败");
-
     }
 
-    public ServerResponse del(Integer userId,Integer shippingId){
+    public ServerResponse<String> del(Integer userId,Integer shippingId){
         int resultCount = shippingMapper.deleteByShippingIdUserId(userId,shippingId);
-        if (resultCount>0){
-            ServerResponse.createBySuccess("删除地址成功");
+        if(resultCount > 0){
+            return ServerResponse.createBySuccess("删除地址成功");
         }
         return ServerResponse.createByErrorMessage("删除地址失败");
     }
 
 
     public ServerResponse update(Integer userId, Shipping shipping){
-
         shipping.setUserId(userId);
-        int rowCount=shippingMapper.insert(shipping);
-        if (rowCount>0){
+        int rowCount = shippingMapper.updateByShipping(shipping);
+        if(rowCount > 0){
             return ServerResponse.createBySuccess("更新地址成功");
         }
-
         return ServerResponse.createByErrorMessage("更新地址失败");
-
     }
+
+    public ServerResponse<Shipping> select(Integer userId, Integer shippingId){
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId,shippingId);
+        if(shipping == null){
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("更新地址成功",shipping);
+    }
+
+
+    public ServerResponse<PageInfo> list(Integer userId,int pageNum,int pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+    }
+
+
+
+
+
+
 
 }
