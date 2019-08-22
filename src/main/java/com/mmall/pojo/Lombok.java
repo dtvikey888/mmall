c2973aa6f59ee8f571bd1ebe0d5cb8f11c83810d
dtvikey888 <dtvikey@163.com>
2019-08-22 08:01:41 +0800

    first

---
+++ src/main/java/com/mmall/pojo/Lombok.java
@@ -0,0 +1,14 @@
+package com.mmall.pojo;
+
+public class Lombok {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private String userName;
+
+}
