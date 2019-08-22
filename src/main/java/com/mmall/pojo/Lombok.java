ecd4fa0f4b574aa12a09b99c11932b4a4bff7496
dtvikey888 <dtvikey@163.com>
2019-08-22 08:34:10 +0800

    test

---
+++ src/main/java/com/mmall/pojo/Lombok.java
@@ -0,0 +1,19 @@
+package com.mmall.pojo;
+
+/**
+ * @Author: dtvikey
+ * @Date: 21/08/19 下午 04:27
+ * @Version 1.0
+ */
+public class Lombok {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
